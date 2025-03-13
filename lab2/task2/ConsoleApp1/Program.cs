87a1a45a48f42727deb5b689e431f11bd347048d
xofoxe <ipz233_fmp@student.ztu.edu.ua>
2025-03-13 11:55:32 +0200

    Update Program.cs

--- lab2/task2/ConsoleApp1/Program.cs
+++ lab2/task2/ConsoleApp1/Program.cs
@@ -1,64 +1,121 @@
-using System;
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
 
-interface IDevice1
+using System; 
+
+public interface Laptop
+{
+    public string GetDeviceType();
+}
+
+public class IProneLaptop : Laptop
+{
+    public string GetDeviceType() => "IProneLaptop";
+}
+
+public class KiaomiLaptop : Laptop
+{
+    public string GetDeviceType() => "KiaomiLaptop";
+}
+ 
+public class BalaxyLaptop : Laptop
+{
+    public string GetDeviceType() => "BalaxyLaptop";
+}
+ 
+ 
+public interface Netbook
 {
     string GetDeviceType();
 }
-interface IDevice2
+
+class IProneNetbook : Netbook
+{
+    public string GetDeviceType() => "IProneNetbook";
+}
+
+class KiaomiNetbook : Netbook
+{
+    public string GetDeviceType() => "KiaomiNetbook";
+}
+ 
+class BalaxyNetbook : Netbook
+{
+    public string GetDeviceType() => "BalaxyNetbook";
+}
+ 
+ 
+interface EBook
 {
     string GetDeviceType();
 }
 
-class Laptop : IDevice1
+class IProneEBook : EBook
 {
-    public string GetDeviceType() => "Laptop";
+    public string GetDeviceType() => "IProneEBook";
 }
 
-class Netbook : IDevice1
+class KiaomiEBook : EBook
 {
-    public string GetDeviceType() => "Netbook";
+    public string GetDeviceType() => "KiaomiEBook";
+}
+ 
+class BalaxyEBook : EBook
+{
+    public string GetDeviceType() => "BalaxyEBook";
 }
 
-class EBook : IDevice2
+interface Smartphone
 {
-    public string GetDeviceType() => "EBook";
+    string GetDeviceType();
 }
 
-class Smartphone : IDevice2
+class IProneSmartphone : Smartphone
 {
-    public string GetDeviceType() => "Smartphone";
+    public string GetDeviceType() => "IProneSmartphone";
 }
+
+class KiaomiSmartphone : Smartphone
+{
+    public string GetDeviceType() => "KiaomiSmartphone";
+}
+ 
+class BalaxySmartphone : Smartphone
+{
+    public string GetDeviceType() => "BalaxySmartphone";
+}
+
 
 interface IDeviceFactory
 {
-    IDevice1 CreateLaptop();
-    IDevice1 CreateNetbook();
-    IDevice2 CreateEBook();
-    IDevice2 CreateSmartphone();
+  public  Laptop CreateLaptop();
+   public Netbook CreateNetbook();
+   public EBook CreateEBook();
+   public Smartphone CreateSmartphone();
 }
 
 class IProneFactory : IDeviceFactory
 {
-    public IDevice1 CreateLaptop()=>new Laptop();    
-    public IDevice1 CreateNetbook() => new Netbook();
-    public IDevice2 CreateEBook() => new EBook();
-    public IDevice2 CreateSmartphone() => new Smartphone();
+   public Laptop CreateLaptop()=> new IProneLaptop();
+   public Netbook CreateNetbook()=> new IProneNetbook();
+   public EBook CreateEBook()=> new IProneEBook();
+   public Smartphone CreateSmartphone()=> new IProneSmartphone();
 }
 
 class KiaomiFactory : IDeviceFactory
 {
-    public IDevice1 CreateLaptop() => new Laptop();
-    public IDevice1 CreateNetbook() => new Netbook();
-    public IDevice2 CreateEBook() => new EBook();
-    public IDevice2 CreateSmartphone() => new Smartphone();
+     public  Laptop CreateLaptop() => new KiaomiLaptop();
+   public Netbook CreateNetbook()=> new KiaomiNetbook();
+   public EBook CreateEBook()=> new KiaomiEBook();
+   public Smartphone CreateSmartphone()=> new KiaomiSmartphone();
 }
 
 class BalaxyFactory : IDeviceFactory
 {
-    public IDevice1 CreateLaptop() => new Laptop();
-    public IDevice1 CreateNetbook() => new Netbook();
-    public IDevice2 CreateEBook() => new EBook();
-    public IDevice2 CreateSmartphone() => new Smartphone();
+     public  Laptop CreateLaptop() => new BalaxyLaptop();
+   public Netbook CreateNetbook()=> new BalaxyNetbook();
+   public EBook CreateEBook()=> new BalaxyEBook();
+   public Smartphone CreateSmartphone()=> new BalaxySmartphone();
 }
 
 class Program
